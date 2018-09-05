137ffd25de84695481e0399e5e8bc59363b302f4
wangcheng10 <wangcheng10@xiaomi.com>
2018-09-05 18:22:41 +0800

    init hzau bbs

---
+++ src/main/java/org/feidian/hzau/bbs/BBSApplicationStartUp.java
@@ -0,0 +1,13 @@
+package org.feidian.hzau.bbs;
+
+import com.jfinal.core.JFinal;
+
+/**
+ * @author wangcheng10@xiaomi.com
+ * @create 18-9-5
+ */
+public class BBSApplicationStartUp {
+    public static void main(String[] args) {
+        JFinal.start("src/main/webapp", 8888, "/");
+    }
+}
---
+++ src/main/java/org/feidian/hzau/bbs/config/AppConfig.java
@@ -0,0 +1,46 @@
+package org.feidian.hzau.bbs.config;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.template.Engine;
+
+/**
+ * @author wangcheng10@xiaomi.com
+ * @create 18-9-5
+ */
+public class AppConfig extends JFinalConfig {
+
+    @Override
+    public void configConstant(Constants me) {
+
+    }
+
+    @Override
+    public void configRoute(Routes me) {
+
+    }
+
+    @Override
+    public void configEngine(Engine me) {
+
+    }
+
+    @Override
+    public void configPlugin(Plugins me) {
+
+    }
+
+    @Override
+    public void configInterceptor(Interceptors me) {
+
+    }
+
+    @Override
+    public void configHandler(Handlers me) {
+
+    }
+}
